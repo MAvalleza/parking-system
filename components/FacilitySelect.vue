03f29f66a4c68029050f5c69d2e374794caade05
Marc Almond Valleza <majvalleza@gmail.com>
2022-02-05 23:15:06 +0800

    feat: setup index page

---
+++ components/FacilitySelect.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+  div
+    v-select(
+      :items="facilities"
+      item-text="name"
+      item-value="id"
+      outlined
+      label="Select Parking Facility"
+      @change="onSelect($event)"
+    )
+    v-btn(
+      color="primary"
+      depressed
+      large
+      @click="onCreate"
+    ).text-none Create new Parking Facility
+</template>
+
+<script>
+export default {
+  props: {
+    facilities: {
+      type: Array,
+      default: () => ([]),
+    },
+  },
+  methods: {
+    onSelect (facility) {
+      this.$emit('select', facility);
+    },
+    onCreate () {
+      this.$emit('create');
+    },
+  },
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,33 @@
 <template lang="pug">
-  div
+  v-container.fill-height
+    v-row(align="center")
+      v-col(cols="12" md="4" xl="3")
+        facility-select(
+          :facilities="parkingFacilities"
+          @create="setupCreateParking"
+        )
 </template>
 
 <script>
-
+import FacilitySelect from '~/components/FacilitySelect';
 export default {
-  asyncData ({ $fire }) {
+  components: {
+    FacilitySelect,
+  },
+  async asyncData ({ $fire }) {
     const snapshot = await $fire.firestore.collection('parking-facilities').get();
-    const parkingFacilities = snapshot.docs.map(doc => doc.data());
-    return parkingFacilities;
-  }
+    const parkingFacilities = snapshot.docs.map(doc => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+    return { parkingFacilities };
+  },
+  methods: {
+    setupCreateParking () {
+      this.$router.push({
+        name: 'create-parking',
+      });
+    },
+  },
 };
 </script>
