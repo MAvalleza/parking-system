ba10e7517925d0f6125fbedb98cdfd0fb301500d
Marc Almond Valleza <majvalleza@gmail.com>
2022-02-05 22:45:32 +0800

    refactor: update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,18 +2,25 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   parserOptions: {
-    parser: '@babel/eslint-parser',
-    requireConfigFile: false
+    parser: 'babel-eslint',
   },
   extends: [
     '@nuxtjs',
     'plugin:nuxt/recommended',
   ],
-  plugins: [
-  ],
   // add your custom rules here
-  rules: {}
-}
+  rules: {
+    'nuxt/no-cjs-in-config': 'off',
+    semi: [2, 'always'],
+    'space-before-function-paren': [2, 'always'],
+    'keyword-spacing': [2, { before: true, after: true }],
+    'space-before-blocks': [2, 'always'],
+    'comma-dangle': [2, 'always-multiline'],
+    'no-console': 'off',
+    'no-multi-str': 'off',
+    curly: ['error', 'multi-line'],
+  },
+};
