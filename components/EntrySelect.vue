1ecb993c06f12bf7ae74499c17ca1a374d88ee92
Marc Almond Valleza <majvalleza@gmail.com>
2022-02-07 00:59:47 +0800

    feat: add parking date and time picker

--- components/EntrySelect.vue
+++ components/EntrySelect.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-  div.d-flex
+  div
     v-select(
       v-model="selectedEntry"
       :items="entries"
@@ -7,22 +7,34 @@
       outlined
       dense
       label="Select Entry Point"
-    ).mr-5
+    )
       template(slot="selection" slot-scope="data")
         span Entry {{ data.item.entryNo }}
       template(slot="item" slot-scope="data")
         span Entry {{ data.item.entryNo }}
-    v-btn(
-      color="success"
-      depressed
-      large
-      :disabled="!selectedEntry"
-      @click="onPark"
-    ).text-none PARK
+    div.d-flex
+      //- Date Time
+      date-time-picker(
+        datetime
+        outlined
+        dense
+        label="Parking Date and Time"
+      )
+      v-btn(
+        color="success"
+        depressed
+        large
+        :disabled="!selectedEntry"
+        @click="onPark"
+      ).text-none.ml-5 PARK
 </template>
 
 <script>
+import DateTimePicker from './DateTimePicker';
 export default {
+  components: {
+    DateTimePicker,
+  },
   props: {
     entries: {
       type: Array,
