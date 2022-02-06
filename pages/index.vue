6589a2c051803850ea6d365242ad0ac4c600fd19
Marc Almond Valleza <majvalleza@gmail.com>
2022-02-06 22:34:55 +0800

    refactor: load data in index

--- pages/index.vue
+++ pages/index.vue
@@ -4,8 +4,17 @@
       v-col(cols="12" md="4" xl="3")
         facility-select(
           :facilities="parkingFacilities"
+          @select="onSelectFacility($event)"
           @create="setupCreateParking"
         )
+      v-col(cols="12" md="4" xl="3")
+    v-row(
+      v-if="showSystem"
+      align="center"
+    )
+      v-col(cols="12")
+        h3 System here
+        pre {{ parkingSlots }}
 </template>
 
 <script>
@@ -15,18 +24,73 @@ export default {
     FacilitySelect,
   },
   async asyncData ({ $fire }) {
-    const snapshot = await $fire.firestore.collection('parking-facilities').get();
-    const parkingFacilities = snapshot.docs.map(doc => ({
+    const db = $fire.firestore;
+    const facilitySnapshot = await db.collection('parking-facilities').get();
+    const parkingFacilities = facilitySnapshot.docs.map(doc => ({
       id: doc.id,
       ...doc.data(),
     }));
-    return { parkingFacilities };
+    const vehicleSnapshot = await db.collection('vehicles').get();
+    const vehicles = vehicleSnapshot.docs.map(doc => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+    return { parkingFacilities, vehicles };
+  },
+  data () {
+    return {
+      showSystem: false,
+      //
+      selectedFacilityId: null,
+      parkingEntries: [],
+      parkingSlots: [],
+    };
   },
   methods: {
     setupCreateParking () {
       this.$router.push({
         name: 'create-parking',
       });
+    },
+    onSelectFacility (facilityId) {
+      console.log('selected facility', facilityId);
+      this.selectedFacilityId = facilityId;
+      this.loadData();
+    },
+    async loadData () {
+      try {
+        await Promise.all([
+          this.loadParkingEntries(),
+          this.loadParkingSlots(),
+        ]);
+        this.showSystem = true;
+      } catch (e) {
+        console.error(e);
+      }
+    },
+    async loadParkingEntries () {
+      const db = this.$fire.firestore;
+      const snapshot = await db.collection('parking-entries')
+        .where('facility', '==', this.selectedFacilityId)
+        .orderBy('entryNo')
+        .get();
+      this.parkingEntries = snapshot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      console.log('loaded entries', this.parkingEntries);
+    },
+    async loadParkingSlots () {
+      const db = this.$fire.firestore;
+      const snapshot = await db.collection('parking-slots')
+        .where('facility', '==', this.selectedFacilityId)
+        .orderBy('slotNo')
+        .get();
+      this.parkingSlots = snapshot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      console.log('loaded slots', this.parkingSlots);
     },
   },
 };
